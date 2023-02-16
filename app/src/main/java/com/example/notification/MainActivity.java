d7cc976c91a73277f4cd9ada54fc3370d8fd8804
JohnsonAudreyRalph <kalilinuxhackmaster@gmail.com>
2023-02-16 21:18:22 +0700

    Tạo ra được 1 notification với việc bấm nút thì sẽ nhận được 1 notification (Nhưng chưa làm được phần Sound)

--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View buttonNotify = findViewById(R.id.buttonNotify);
+        NotificationChannel channel;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_HIGH);
+            channel.setLightColor(Color.GRAY);
+            channel.enableLights(true);
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .build();
+            channel.setSound(uri, audioAttributes);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        buttonNotify.setOnClickListener(v -> sendNotification());
+    }
+    public Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sound_1);
+    private void sendNotification() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "My Notification");
+        builder.setSmallIcon(R.drawable.icons8_kali_linux);
+        builder.setLargeIcon(bitmap);
+        builder.setSound(uri);
+        builder.setContentTitle("New Message");
+        builder.setContentText("Chúc mừng bạn đã tạo một thông báo mới thành công");
+        builder.setAutoCancel(true);
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+        managerCompat.notify(1, builder.build());
     }
 }
